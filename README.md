535af386d60c2261c57c80af696901b47ee0e554
Alisa Sanakoeva <alisa.sanakoevi.1@iliauni.edu.ge>
2020-08-14 18:46:29 +0400

    ჰეროკუსთვის პროექტის გამზადება

---
+++ README.md
@@ -0,0 +1,9 @@
+# Unilab Flask Server [demo]
+This is Flask Web Server for my project "snakes"
+
+## Documentation
+Snake database
+
+**Method**:
+POST : _/snakes_
+`price` type: float
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_restful import Api
 from flask_jwt import JWT
 
@@ -19,13 +19,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 jwt = JWT(app, authentification, identity)
 api = Api(app)
 
+@app.route('/')
+def redirect2url():
+    return redirect("https://github.com/alisa-sanakoeva/snakes-of-Georgia")
+
 api.add_resource(Item, '/snakes/<string:species>')
 api.add_resource(Itemlist, '/snakes')
 api.add_resource(Registration, '/registration/')
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 if __name__ == "__main__":
     from db import db
