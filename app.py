9f675ca8bebbb8977c8e2733c1db993ca4d8c0ef
airmckay <apmc@nmbu.no>
2022-11-17 11:02:28 +0100

    new branch attempt to merge

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from resources.user import Registration
 
 from security import authentification, identity
 #hi from future me
-
+#new one
 
 app = Flask(__name__)
 app.secret_key = "წენგოსფერი მცურავი საუკეთესო გველია"
